42b680f8ea8c54df01e629d2a31e1351cce0ae98
Victor Perez <vaperezr@unal.edu.co>
2017-12-10 16:35:43 -0500

    feature: Creates a post component, and updates the Api methods

--- src/Api.js
+++ src/Api.js
@@ -5,11 +5,6 @@ const api = 'http://localhost:3001'
 
 let token = localStorage.token
 
-if (!token)
-    token = localStorage.token = Math.random()
-        .toString(36)
-        .substr(-8)
-
 const headers = {
     Authorization: token
 }
@@ -32,4 +27,40 @@ export const getCategoryPosts = category =>
 
 export const createPost = data =>
     axios.post(`${api}/posts`, data, { headers })
-    .then(data => data.data)
+        .then(data => data.data)
+
+export const votePost = (id, vote) =>
+    axios.post(`${api}/posts/${id}`, vote, { headers })
+        .then(data => data.data)
+
+export const editPost = (id, data) =>
+    axios.put(`${api}/posts/${id}`, data, { headers })
+        .then(data => data.data)
+
+export const deletePost = id =>
+    axios.delete(`${api}/posts/${id}`, { headers })
+        .then(data => data.data)
+
+export const getPostComments = id =>
+    axios.get(`${api}/posts/${id}/comments`, { headers })
+        .then(data => data.data)
+
+export const commentPost = data =>
+    axios.post(`${api}/comments`, data, { headers })
+        .then(data => data.data)
+
+export const getComment = id =>
+    axios.get(`${api}/comments/${id}`, { headers })
+        .then(data => data.data)
+
+export const voteComment = (id, vote) =>
+    axios.post(`${api}/comments/${id}`, vote, { headers })
+        .then(data => data.data)
+
+export const deleteComment = id =>
+    axios.delete(`${api}/comments/${id}`, { headers })
+        .then(data => data.data)
+
+export const editComment = (id, data) =>
+    axios.put(`${api}/comments/${id}`, data, { headers })
+        .then(data => data.data)
--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import uuidv1 from 'uuid/v1'
+import uuidv1 from 'uuid/v1' // eslint-disable-line no-unused-vars
 import * as Api from './Api' // eslint-disable-line no-unused-vars
+
+class Post extends Component {
+  render() {
+    const { post } = this.props
+
+    return (
+      <div className="post" key={ post.key }>
+        <h2>{ post.title }</h2>
+        <div className="post-vote">{ post.postVote}</div>
+        <div className="post-body">
+          <p>
+            { post.body }
+          </p>
+        </div>
+        <div className="post-author">{ post.author }</div>
+        <div className="post-date">{ new Date(post.timestamp).toLocaleString() }</div>
+
+        <div className="post-comment-count">{ post.commentCount}</div>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   state = {
@@ -9,12 +31,18 @@ class App extends Component {
     posts: [],
     category: [],
     post: {},
-    newpost: {}
+    newpost: {},
+    postVote: '',
+    comments: [],
+    postComment: {},
+    commentVote: ''
   }
 
 	componentDidMount () {
     let category = 'react'
-    let data = {
+
+    //data required to create a post
+    /* let data = {
       id: uuidv1(),
       title: 'React is a great way to improve javascript skills',
       body: 'This project helps me to get better at react',
@@ -24,11 +52,45 @@ class App extends Component {
       voteScore: 0,
       commentCount: 0,
       timestamp: Date.now()
+    } */
+
+    //commentData
+    /* let commentData = {
+      id: uuidv1(),
+      parentId: '2aabe9c0-ddb8-11e7-ae7f-b92c97863714',
+      body: 'This is a comment test',
+      author: 'victorperez',
+      timestamp: Date.now()
+    } */
+
+    let editData = {
+      title: 'React is a great way to improve my javascript skills',
+      body: 'This project is helping me to get better at react and redux'
     }
 
+    let editCommentData = {
+      timestamp: Date.now(),
+      body: 'This is an edited comment'
+    }
+
+    let option = {
+      option: 'upVote'
+    }
+
+    //create post, requires the post data
     /* Api.createPost(data).then((newpost) => {
       this.setState({ newpost })
     }) */
+
+    //delete post, requires the post ID
+    /* Api.deletePost('identificador123').then((deletedPost) => {
+      console.log({ deletedPost })
+    }) */
+
+    //votePost requires the post id and an option upVote or downVote
+    Api.votePost( '2aabe9c0-ddb8-11e7-ae7f-b92c97863714', option ).then((postVote) => {
+      this.setState({ postVote })
+    })
 
 		Api.getCategories().then((categories) => {
 			this.setState({ categories })
@@ -41,9 +103,38 @@ class App extends Component {
     Api.getCategoryPosts(category).then((category) => {
 			this.setState({ category })
     })
-    
+
+    //editPost requires the post id and the edit data, 
+    Api.editPost('2aabe9c0-ddb8-11e7-ae7f-b92c97863714', editData).then((newpost) => {
+      //console.log({ newpost })
+    })
+
+    //
+    /* Api.commentPost(commentData).then((postComment) => {
+      console.log({ postComment })
+    }) */
+
+    //delete comment, requires the comment ID
+    /* Api.deleteComment('828329a0-dde6-11e7-9599-e373cf1963cb').then((deletedComment) => {
+      console.log({ deletedComment })
+    }) */
+
+    //votePost requires the post id and an option upVote or downVote
+    Api.voteComment( 'eb118520-dde6-11e7-af31-979f316ae76d', option ).then((commentVote) => {
+      this.setState({ commentVote })
+    })
+
+    //editComment requires the comment id and the edit data, 
+    Api.editComment('d680eec0-dde6-11e7-874f-6bf0c092fb1e', editCommentData).then((newpost) => {
+      //console.log({ newpost })
+    })
+
     Api.getSinglePost("2aabe9c0-ddb8-11e7-ae7f-b92c97863714").then((post) => {
 			this.setState({ post })
+    })
+
+    Api.getPostComments('2aabe9c0-ddb8-11e7-ae7f-b92c97863714').then((comments) => {
+      this.setState({ comments })
     })
   }
 
@@ -51,7 +142,7 @@ class App extends Component {
     let categories = this.state.categories
     let posts = this.state.posts
     let category = this.state.category
-    let post = this.state.post
+    let comments = this.state.comments
 
     return (
       <div>
@@ -67,55 +158,39 @@ class App extends Component {
         </ul>
 
         <h3>POSTS</h3>
-        <ul>
           {
             posts.map((value) => (
-              <li key={value.id}>
-                <span>ID: {value.id}</span><br></br>
-                <span>TITLE: {value.title}</span><br></br>
-                <span>BODY: {value.body}</span><br></br>
-                <span>CATEGORY: {value.category}</span><br></br>
-                <span>AUTHOR: {value.author}</span><br></br>
-                <span>COMMENTCOUNT: {value.commentCount}</span><br></br>
-                <span>DELETED: {value.deleted}</span><br></br>
-                <span>VOTESCORE: {value.voteScore}</span><br></br>
-              </li>
+              <Post post={ value }/>
             ))
           }
-        </ul>
 
         <h3>CATEGORY POSTS</h3>
-        <ul>
           {
             category.map((value) => (
-              <li key={value.id}>
-                <span>TITLE: {value.title}</span><br></br>
-                <span>BODY: {value.body}</span><br></br>
-                <span>CATEGORY: {value.category}</span><br></br>
-                <span>AUTHOR: {value.author}</span><br></br>
-                <span>COMMENTCOUNT: {value.commentCount}</span><br></br>
-                <span>DELETED: {value.deleted}</span><br></br>
-                <span>VOTESCORE: {value.voteScore}</span><br></br>
-              </li>
+              <Post post={ value }/>
             ))
           }
-        </ul>
 
         <h3>POST</h3>
+        <Post post={this.state.post}/>
+
+        <h3>COMMENTS</h3>
         <ul>
           {
-              <li key={post.id}>
-                <span>TITLE: {post.title}</span><br></br>
-                <span>BODY: {post.body}</span><br></br>
-                <span>CATEGORY: {post.category}</span><br></br>
-                <span>AUTHOR: {post.author}</span><br></br>
-                <span>COMMENTCOUNT: {post.commentCount}</span><br></br>
-                <span>DELETED: {post.deleted}</span><br></br>
-                <span>VOTESCORE: {post.voteScore}</span><br></br>
+              comments.map((comment) => (
+                <li key={comment.id}>
+                <span>BODY: {comment.body}</span><br></br>
+                <span>AUTHOR: {comment.author}</span><br></br>
+                <span>DELETED: {comment.deleted}</span><br></br>
+                <span>ID: {comment.id}</span><br></br>
+                <span>PARENTDELETED: {comment.parentDeleted}</span><br></br>
+                <span>PARENTID: {comment.parentId}</span><br></br>
+                <span>TIMESTAMP: { new Date(comment.timestamp).toLocaleString() }</span><br></br>
+                <span>VOTESCORE: {comment.voteScore}</span><br></br>
               </li>
+              ))
           }
         </ul>
-        
       </div>
     );
   }
